0ba8337ef364f19d89a8121256907765c8a3543f
wtv-filipa <anafilipa.ferreira99@ua.pt>
2020-04-23 12:42:40 +0100

    novas páginas, todas as opções disponíveis e posso escolher de estudar e trabalhar

---
+++ HiLives/public/allOptions.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- metadados -->
+    <?php include "helpers/meta.php"; ?>
+    <title>Todas as opções</title>
+     <!-- Custom fonts for this template-->
+    <?php include "helpers/fonts.php"; ?>
+    <!-- Custom styles for this template-->
+    <?php include "helpers/css_allOptions.php"; ?>
+
+</head>
+
+<body>
+    <header class="sticky-top">
+        <!--navbar-->
+        <?php include "components/navbar.php"; ?>
+    </header>
+    <main class="container p-0 mb-5 mx-auto">
+        <!--componente da home sem login-->
+        <?php include "components/allOptions.php"; ?>
+    </main>
+    <!-- JavaScript-->
+    <?php include "helpers/js.php"; ?>
+    <?php include "helpers/fontawesome.php"; ?>
+</body>
+
+</html>
---
+++ HiLives/public/components/allOptions.php
@@ -0,0 +1,181 @@
+<!--Universidades-->
+<div class=" mx-auto w-75">
+    <div id='wrapper_title' class="mb-4">
+        <div class='tagpost-top section' id='tagpost-top'>
+            <div class='widget HTML' id='HTML5'>
+                <div>
+                    <h3 class="main_title">Todas as opções disponíveis</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="card-deck text-center row">
+
+        <div class="cards col-s-12 col-col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg">
+                </div>
+                <div class="card-info">
+                    <h4 class="card-intro description_title"><i class="fas fa-book-open" style="color: #2f2f2f;"></i> Estudar</h4>
+                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
+                    <p class="card-intro description_title">Ver áreas disponíveis</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/fnac.jpg">
+                </div>
+                <div class="card-info work">
+                    <h4 class="card-intro description_title"> <i class="fas fa-suitcase" style="color: #2f2f2f;"></i> Trabalhar</h4>
+                    <h2 class="card-title sub_title">Fnac</h2>
+                    <a href="vacancie.php">
+                        <p class="card-intro description_title">Ver mais informações</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg"></div>
+                <div class="card-info">
+                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
+                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
+                    <p class="card-intro description_title">Ver áreas disponíveis</p>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg"></div>
+                <div class="card-info work">
+                    <h4 class="card-intro description_title"> <i class="fas fa-suitcase" style="color: #2f2f2f;"></i> Trabalhar</h4>
+                    <h2 class="card-title sub_title">Fnac</h2>
+                    <a href="vacancie.php">
+                        <p class="card-intro description_title">Ver mais informações</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg"></div>
+                <div class="card-info">
+                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
+                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
+                    <p class="card-intro description_title">Ver áreas disponíveis</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg"></div>
+                <div class="card-info work">
+                    <h4 class="card-intro description_title"> <i class="fas fa-suitcase" style="color: #2f2f2f;"></i> Trabalhar</h4>
+                    <h2 class="card-title sub_title">Fnac</h2>
+                    <a href="vacancie.php">
+                        <p class="card-intro description_title">Ver mais informações</p>
+                    </a>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg"></div>
+                <div class="card-info">
+                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
+                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
+                    <p class="card-intro description_title">Ver áreas disponíveis</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg">
+                </div>
+                <div class="card-info work">
+                    <h4 class="card-intro description_title"> <i class="fas fa-suitcase" style="color: #2f2f2f;"></i> Trabalhar</h4>
+                    <h2 class="card-title sub_title">Fnac</h2>
+                    <a href="vacancie.php">
+                        <p class="card-intro description_title">Ver mais informações</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg"></div>
+                <div class="card-info work">
+                    <h4 class="card-intro description_title"> <i class="fas fa-suitcase" style="color: #2f2f2f;"></i> Trabalhar</h4>
+                    <h2 class="card-title sub_title">Fnac</h2>
+                    <a href="vacancie.php">
+                        <p class="card-intro description_title">Ver mais informações</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg"></div>
+                <div class="card-info">
+                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
+                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
+                    <p class="card-intro description_title">Ver áreas disponíveis</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg"></div>
+                <div class="card-info">
+                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
+                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
+                    <p class="card-intro description_title">Ver áreas disponíveis</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="cards col-s-12 col-sm-4">
+            <div class="card-item">
+                <div class="card-image">
+                    <img class="imagem" src="img/ua.jpg"></div>
+                <div class="card-info">
+                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
+                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
+                    <p class="card-intro description_title">Ver áreas disponíveis</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!--fim do que engloba os cards-->
+</div> <!-- div da w-75-->
