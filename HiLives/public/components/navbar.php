115ee9e5a62d9623130f7f061e50fec8e1e69f69
Fred <fredericoproenca@ua.pt>
2020-04-28 19:24:39 +0100

    nav atualizada com o "inicia sessao" no index e cores da home corrigidas

--- HiLives/public/components/navbar.php
+++ HiLives/public/components/navbar.php
@@ -28,27 +28,48 @@ $stmt = mysqli_stmt_init($link);
 
 <header class="top">
     <nav class="nav__ container cont_l">
-        <div class="nav__controls--left">
-            <a href="#" class="nav__link">
-                <i class="fas fa-ellipsis-v" id="button"
-                   style="margin-left:20px; color: #2f2f2f; font-size: 25px !important;">
-                    <span class="menunav ml-1">Eu quero</span>
-                </i>
-            </a>
-        </div>
+            <div class="nav__controls--left">
+                <?php
+                if (isset($_SESSION["idUser"])) {
+                ?>
+                <a href="#" class="nav__link">
+                    <i class="fas fa-ellipsis-v" id="button"
+                       style="margin-left:20px; color: #2f2f2f; font-size: 25px !important;">
+                        <span class="menunav ml-1">Eu quero</span>
+                    </i>
+                </a>    <?php
+                }
+                ?>
+            </div>
+
 
         <div class="nav__brand">
             <a class="nav__link" href="index.php">
                 <img src="img/logo.png" class="img-responsive" style="width:90px; margin-right: 5px">
             </a>
         </div>
+
         <div class="nav__controls nav__controls--right">
+            <?php
+            if (!isset($_SESSION["idUser"])) {
+                ?>
+            <div class="text-center nav__avatar">
+                <a href="login.php">
+                    <button class="btn inicia" style="width: 100% !important;">Inicia Sessão</button>
+                </a>
+            </div>
+            <?php
+            }
+            ?>
             <!-- <div class="nav__search">
                  <input class="nav__search--input text-center" placeholder="Pesquisar..." type="text"/><a class="nav__search--icon " href="#"><i class="fa fa-bell-o" style="font-size: 21px"></i></a>
              </div> -->
+            <?php
+
+            if (isset($_SESSION["idUser"])) {
+                ?>
             <div class="nav__avatar">
-                <img src="img/notif.png" class=" " style="position:relative; max-width:30px"><span class="nome ml-2"
-                                                                                                   style=" width: 180px; color: black">O que está a acontecer</span>
+                <img src="img/notif.png" class=" " style="position:relative; max-width:30px"><span class="nome ml-2" style=" width: 180px; color: black">O que está a acontecer</span>
 
                 <div class="nav__avatar--dropdown" style="overflow-y: scroll; width: 400px">
 
@@ -96,6 +117,9 @@ $stmt = mysqli_stmt_init($link);
                         </button>
                     </a>
                 </div>
+                <?php
+                }
+                ?>
             </div>
             <?php
 
@@ -114,14 +138,14 @@ $stmt = mysqli_stmt_init($link);
             <div class="nav__avatar">
                 <img src="../admin/uploads/img_perfil/<?= $img_perfil ?>" class="nav__avatar--image "
                      style="max-width:35px">
-                <span class="nome ml-3" style="color: black"><?= $name_user ?></span>
+                <span class="nome ml-2" style="color: black"><?= $name_user ?></span>
 
                 <?php
                 } else{
                 ?>
                 <div class="nav__avatar">
                     <img src="img/no_profile_img.png" class="nav__avatar--image " style="max-width:35px">
-                    <span class="nome ml-3" style="color: black"><?= $name_user ?></span>
+                    <span class="nome ml-2" style="color: black"><?= $name_user ?></span>
                     <?php
                     }
             }
@@ -208,95 +232,96 @@ $stmt = mysqli_stmt_init($link);
                 </div>
     </nav>
 
-    <div id="slide1"><a href="#" id="button2"><i class="fa fa-times" style="color: black"></i>
-            <ul class="">
-                <!-- EU QUERO ESTUDAR -->
-                <li>
-                    <a class="nav__link mr-2 mb-1">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text_main font-weight-bold">Eu quero estudar</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="can_choose_study.php" class="nav__link mr-2 mb-1 zoom">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text">As minha ligações</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="allOptions_uni.php" class="nav__link mr-2 mb-1 zoom">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text">Todas as opções disponíveis</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="links_chosen.php" class="nav__link mr-2 mb-1 zoom">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text">As minhas escolhas</span>
-                    </a>
-                </li>
+        <div id="slide1"><a href="#" id="button2"><i class="fa fa-times" style="color: black"></i>
+                <ul class="">
+                    <!-- EU QUERO ESTUDAR -->
+                    <li>
+                        <a class="nav__link mr-2 mb-1">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text_main font-weight-bold">Eu quero estudar</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="can_choose_study.php" class="nav__link mr-2 mb-1 zoom">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text">As minha ligações</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="allOptions_uni.php" class="nav__link mr-2 mb-1 zoom">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text">Todas as opções disponíveis</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="links_chosen.php" class="nav__link mr-2 mb-1 zoom">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text">As minhas escolhas</span>
+                        </a>
+                    </li>
 
-                <!-- EU QUERO TRABALHAR -->
-                <li>
-                    <a class="nav__link mr-2 mb-1 mt-4">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text_main font-weight-bold">Eu quero trabalhar</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="can_choose_work.php" class="nav__link mr-2 mb-1 zoom">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text">As minhas ligações</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="allOptions_company.php" class="nav__link mr-2 mb-1 zoom">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text">Todas as opções disponíveis</span>
-                    </a>
-                </li>
+                    <!-- EU QUERO TRABALHAR -->
+                    <li>
+                        <a class="nav__link mr-2 mb-1 mt-4">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text_main font-weight-bold">Eu quero trabalhar</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="can_choose_work.php" class="nav__link mr-2 mb-1 zoom">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text">As minhas ligações</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="allOptions_company.php" class="nav__link mr-2 mb-1 zoom">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text">Todas as opções disponíveis</span>
+                        </a>
+                    </li>
 
-                <!-- EU QUERO ESTUDAR E TRABALHAR -->
-                <li>
-                    <a class="nav__link mr-2 mb-1 mt-4">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text_main font-weight-bold">Eu quero estudar e trabalhar</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="can_choose_ws.php" class="nav__link mr-2 mb-1 zoom">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text">As minhas ligações</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="allOptions.php" class="nav__link mr-2 mb-1 zoom">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text">Todas as opções disponíveis</span>
-                    </a>
-                </li>
+                    <!-- EU QUERO ESTUDAR E TRABALHAR -->
+                    <li>
+                        <a class="nav__link mr-2 mb-1 mt-4">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text_main font-weight-bold">Eu quero estudar e trabalhar</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="can_choose_ws.php" class="nav__link mr-2 mb-1 zoom">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text">As minhas ligações</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="allOptions.php" class="nav__link mr-2 mb-1 zoom">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text">Todas as opções disponíveis</span>
+                        </a>
+                    </li>
 
-                <!-- EU QUERO VER O QUE JÁ FOI FEITO -->
-                <li>
-                    <a class="nav__link mr-2 mb-1 mt-4">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text_main font-weight-bold">Eu quero ver o que já foi feito</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="links_made.php" class="nav__link mr-2 mb-1 zoom">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text">O que eu já fiz</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="experiences.php" class="nav__link mr-2 mb-1 zoom">
-                        <img src="" alt="" class="icones nav__link--icon"/>
-                        <span class="nav__link--text">O que os outros fizeram</span>
-                    </a>
-                </li>
-            </ul>
-    </div>
+                    <!-- EU QUERO VER O QUE JÁ FOI FEITO -->
+                    <li>
+                        <a class="nav__link mr-2 mb-1 mt-4">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text_main font-weight-bold">Eu quero ver o que já foi feito</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="links_made.php" class="nav__link mr-2 mb-1 zoom">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text">O que eu já fiz</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="experiences.php" class="nav__link mr-2 mb-1 zoom">
+                            <img src="" alt="" class="icones nav__link--icon"/>
+                            <span class="nav__link--text">O que os outros fizeram</span>
+                        </a>
+                    </li>
+                </ul>
+        </div>
+
 
     <script>
         var btn = document.getElementById("button");
