deb4c9e469c69f670e3d733dc109f0fd656cb656
Fred <fredericoproenca@ua.pt>
2020-04-30 17:42:14 +0100

    footer

---
+++ HiLives/public/components/footer.php
@@ -0,0 +1,69 @@
+<!-- Site footer -->
+<footer class="site-footer ">
+    <div class="container">
+        <div class="row mx-auto">
+<!--            <div class="col-sm-12 col-md-6">-->
+<!--                <h6>About</h6>-->
+<!--                <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>-->
+<!--            </div>-->
+
+            <div class="col-xs-6 col-md-2 p-0">
+                <h6>Parceiros</h6>
+                <ul class="footer-links">
+                    <li><a href="https://www.ua.pt/" target="_blank">University of Aveiro </a></li>
+                    <li><a href="https://english.hi.is/university_of_iceland" target="_blank">University of Iceland (UI)</a></li>
+                    <li><a href="https://www.usal.es/" target="_blank">University of Salamanca </a></li>
+                    <li><a href="https://www.ugent.be/en" target="_blank">University of Ghent</a></li>
+
+
+                </ul>
+            </div>
+            <div class="col-xs-6 col-md-4 mt-4 pl-0 text-left">
+                <h6></h6>
+                <ul class="footer-links">
+                    <li><a href="https://assol.pt/" target="_blank">ASSOL</a></li>
+                    <li><a href="https://www.formem.org.pt/" target="_blank">FORMEM</a></li>
+                    <li><a href="https://paisemrede.pt/" target="_blank">Pais em Rede</a></li>
+                    <li><a href="https://www.facebook.com/pages/category/Organization/AVISPT21-Associa%C3%A7%C3%A3o-de-Viseu-de-Portadores-de-Trissomia-21-1642598149403790/" target="_blank">AVISPT21</a></li>
+                </ul>
+            </div>
+
+            <div class="col-xs-6 col-md-5 ">
+                <h6>Atalhos</h6>
+                <ul class="footer-links">
+                    <li><a href="accessibility.php">Acessibilidade</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Contribute</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Sitemap</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-6 col-md-1 pr-0">
+                <h6>Outros</h6>
+                <ul class="footer-links">
+                    <li><a href="accessibility.php">Acessibilidade</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Contribute</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Sitemap</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+                <p class="copyright-text">Copyright &copy; HiLives 2020. Todos os direitos reservados.
+                </p>
+            </div>
+
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <ul class="social-icons">
+                    <li><a class="facebook" href="https://www.facebook.com/HiLives_Erasmus-111765073655672/" target="_blank"><i class="fa fa-facebook" style="color: white;"></i></a></li>
+                    <li><a class="twitter" href="https://twitter.com/HiLives_Erasmus" target="_blank"><i class="fa fa-twitter" style="color: white;"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
