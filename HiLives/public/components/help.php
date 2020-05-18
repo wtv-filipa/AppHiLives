2e5ca7b089755ba7cd8f8ff68108f610bea88ad5
Eduardo Soeiro <eduardosoeiro@ua.pt>
2020-05-18 15:59:21 +0100

    Mapa do site e página de ajuda

---
+++ HiLives/public/components/help.php
@@ -0,0 +1,187 @@
+<?php
+include "navbar_2.php";
+?>
+<div class="w-75 mx-auto">
+
+    <!--TÍTULO-->
+    <div class="mt-5">
+        <h1 class="titulo1 ">Ajuda - HiLives</h1>
+    </div>
+
+    <!--PÁGINA INICIAL - SEM INICIO DE SESSÃO-->
+    <div class="mt-5">
+        <h2 class="subtitulo">O que é a HiLives?</h2>
+        <p>A HiLives é uma plataforma que permite a jovens com dificuldades intelectuais e desenvolvimentais
+            poderem frequentar áreas do seu interesse em várias universidades da União Europeia. Para além disto, a
+            HiLives permite também que estes jovens possam encontrar empregadores que correspondam às suas necessidades
+            e,
+            assim, incluí-los numa empresa.</p>
+
+        <p>Se tens dúvidas acerca de como navegares pela aplicação, visita o <b><a href="sitemap.php" target="_blank">Mapa
+                    da Aplicação</a></b>, e descobre como tudo está ordenado.</p>
+
+        <div class="mt-5">
+            <h3 class="subtitulo">Página Inicial - Sem início de sessão</h3>
+
+            <p>Existem <b>três opções de registo</b>:</p>
+            <ol>
+                <li>Jovens</li>
+                <li>Universidades</li>
+                <li>Empresas</li>
+            </ol>
+
+            <p>Além disto será possível, dentro da mesma página, obter mais informações sobre o conteúdo da
+                aplicação.</p>
+
+            <p>Se o jovem, universidade ou empresa já se tiver registado anteriormente, poderá <u><a href="login.php"
+                                                                                                     target="_blank">iniciar
+                        a sua sessão</a></u>
+        </div>
+    </div>
+
+    <!--PÁGINA INICIAL DO JOVEM - COM SESSÃO INICIADA-->
+    <div class="mt-5">
+        <h3 class="subtitulo">Página Inicial do Jovem - Com sessão iniciada</h3>
+
+        <p>Aqui, o jovem terá acesso a <b>duas barras de navegação</b>.</p>
+
+        <p>A <b>primeira</b> contém os menus:</p>
+        <ol>
+            <li>Eu quero</li>
+            <li>"O que está a acontecer" (Notificações)</li>
+            <li>Nome do utilizador</li>
+        </ol>
+
+        <p>A <b>segunda</b> contém os menus:</p>
+        <ol>
+            <li>Quero estudar</li>
+            <li>Quero trabalhar</li>
+            <li>Quero estudar e trabalhar</li>
+            <li>Quero ver o que já foi feito</li>
+        </ol>
+
+        <p><b>NOTA:</b> O menu <b>"Eu quero"</b> possui as <b>mesmas opções</b> que estão presentes na segunda barra de
+            navegação.
+
+        <p>Ainda na página inicial, o jovem poderá ver:</p>
+        <ol>
+            <li>As minhas ligações recentes</li>
+            <li>Destaques</li>
+            <li>Ligações com universidades</li>
+        </ol>
+
+        <h3 class="mt-4">O que são as <b>Ligações</b>?</h3>
+        <p>As "Ligações" simbolizam o encontro de interesses entre o jovem e as universidades, ou entre os jovens
+            e as empresas. Se o jovem tiver caracteristicas que se adequam a uma determinada empresa, e que esta procura
+            num jovem, por exemplo, irá estabelecer-se uma "Ligação". Funciona de igual modo para as universidades.</p>
+
+        <p><b>DICA:</b> Em qualquer página, ao clicar no logótipo da HiLives, o utilizador será automaticamente
+            redirecionado para a
+            página principal.</p>
+    </div>
+
+    <!--PÁGINA INICIAL DAS UNIVERSIDADES - COM SESSÃO INICIADA-->
+    <div class="mt-5">
+        <h3 class="subtitulo">Página Inicial da Universidade - Com sessão iniciada</h3>
+
+        <p>Aqui, as universidades registadas terão acesso, também estas, a <b>duas barras de navegação</b>.</p>
+
+        <p>A <b>primeira</b> contém os menus:</p>
+        <ol>
+            <li>Eu quero</li>
+            <li>"O que está a acontecer" (Notificações)</li>
+            <li>Nome do utilizador</li>
+        </ol>
+
+        <p>A <b>segunda</b> contém os menus:</p>
+        <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li>Experiências</li>
+        </ol>
+
+        <p><b>NOTA:</b> O menu <b>"Eu quero"</b> possui as <b>mesmas opções</b> que estão presentes na segunda barra de
+            navegação.
+
+        <p>Ainda na página inicial, as universidades terão acesso a informações sobre:</p>
+        <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ol>
+
+        <p><b>DICA:</b> Em qualquer página, ao clicar no logótipo da HiLives, o utilizador será automaticamente
+            redirecionado para a
+            página principal.</p>
+    </div>
+
+    <!--PÁGINA INICIAL DAS EMPRESAS - COM SESSÃO INICIADA-->
+    <div class="mt-5">
+        <h3 class="subtitulo">Página Inicial da Empresa - Com sessão iniciada</h3>
+
+        <p>Aqui, as empresas registadas terão também acesso a <b>duas barras de navegação</b>.</p>
+
+        <p>A <b>primeira</b> contém os menus:</p>
+        <ol>
+            <li>Eu quero</li>
+            <li>"O que está a acontecer" (Notificações)</li>
+            <li>Nome do utilizador</li>
+        </ol>
+
+        <p>A <b>segunda</b> contém os menus:</p>
+        <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li>Experiências</li>
+        </ol>
+
+        <p><b>NOTA:</b> O menu <b>"Eu quero"</b> possui as <b>mesmas opções</b> que estão presentes na segunda barra de
+            navegação.
+
+        <p>Ainda na página inicial, as universidades terão acesso a informações sobre:</p>
+        <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ol>
+
+        <p><b>DICA:</b> Em qualquer página, ao clicar no logótipo da HiLives, o utilizador será automaticamente
+            redirecionado para a
+            página principal.</p>
+    </div>
+
+    <!--RODAPÉ-->
+    <div class="mt-5">
+        <h3 class="subtitulo">Rodapé</h3>
+
+        <p>Presente no fim de todas as páginas, a HiLives possui vários links, entre eles:</p>
+        <ol>
+            <li>Lista de parceiros da HiLives</li>
+            <li>Lista de atalhos para acesso rápido</li>
+            <li>Lista de outros links importantes (Acessibilidade, Política de Privacidade e Mapa da Aplicação)</li>
+            <li>Local para colocação de dúvidas ou sugestões</li>
+            <li>Ícones de acesso às redes sociais da HiLives (Facebook e Twitter)</li>
+            <li>Informações de direitos de autor</li>
+        </ol>
+    </div>
+
+    <!--ÍCONE DE ACESSIBILIDADE (CANTO INFERIOR DIREITO)-->
+    <div class="mt-5">
+        <h3 class="subtitulo">Ícone de Acessibilidade (canto inferior direito)</h3>
+
+        <p>No canto inferior direito, presente também em todas as páginas, existe um <b>ícone representativo da acessibilidade</b>.</p>
+
+        <p>Ao ser clicado, vão ser mostradas aos utilizadores várias opções para melhorar a navegação dos mesmos, o que
+            torna a HiLives uma plataforma totalmente inclusiva e de simples navegação.</p>
+    </div>
+
+    <!--TENS DÚVIDAS OU SUGESTÕES?-->
+    <div class="mt-5">
+        <h3 class="subtitulo">Tens dúvidas ou sugestões?</h3>
+
+        <p>Se tiveres alguma dúvida sobre a HiLives, ou uma sugestão para a tornar melhor, <b><a href="question_form.php" target="_blank">envia-nos
+                    um e-mail</a></b>. Teremos todo o gosto em ajudar-te e ler o que tens para nos dizer!</p>
+    </div>
+</div>
