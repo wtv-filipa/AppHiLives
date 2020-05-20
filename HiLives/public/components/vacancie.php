9bc748425a5f03c0dd7f68d9c27a4cfc313a070f
Fred <fredericoproenca@ua.pt>
2020-05-21 00:05:05 +0100

    pagina da vaga updated again

--- HiLives/public/components/vacancie.php
+++ HiLives/public/components/vacancie.php
@@ -4,12 +4,17 @@ include "navbar_2.php";
 require_once("connections/connection.php");
 $link = new_db_connection();
 $stmt = mysqli_stmt_init($link);
-$query = "SELECT vacancie_name, description_vac, number_free_vanc, requirements, Region_idRegion, User_publicou,Content_idContent, Workday_idWorkday, Educ_lvl_idEduc_lvl, Areas_idAreas FROM vacancies";
+$query = "SELECT vacancie_name, description_vac, number_free_vanc, requirements, Region_idRegion, User_publicou,Content_idContent, Workday_idWorkday, Educ_lvl_idEduc_lvl, Areas_idAreas, name_region, Workday_name, name_interested_area, name_education 
+FROM vacancies
+INNER JOIN region ON vacancies.Region_idRegion = region.idRegion
+INNER JOIN workday ON vacancies.Workday_idWorkday = workday.idWorkday
+INNER JOIN areas ON vacancies.Areas_idAreas = areas.idAreas
+INNER JOIN educ_lvl ON vacancies.Educ_lvl_idEduc_lvl = educ_lvl.idEduc_lvl";
 
 if (mysqli_stmt_prepare($stmt, $query)) {
 
     mysqli_stmt_execute($stmt);
-    mysqli_stmt_bind_result($stmt, $vacancie_name, $description_vac, $number_free_vanc, $requirements, $Region_idRegion, $User_publicou, $Content_idContent, $Workday_idWorkday, $Educ_lvl_idEduc_lvl, $Areas_idAreas);
+    mysqli_stmt_bind_result($stmt, $vacancie_name, $description_vac, $number_free_vanc, $requirements, $Region_idRegion, $User_publicou, $Content_idContent, $Workday_idWorkday, $Educ_lvl_idEduc_lvl, $Areas_idAreas, $name_region, $Workday_name, $name_interested_area, $name_education );
 
     if (mysqli_stmt_fetch($stmt)) {
         ?>
@@ -31,18 +36,23 @@ if (mysqli_stmt_prepare($stmt, $query)) {
                 <input type="radio" id="tab3" name="tab-control">
                 <input type="radio" id="tab4" name="tab-control">
                 <ul>
-                    <li title="Features"><label for="tab1" role="button"><svg viewBox="0 0 24 24">
+                    <li title="Informações"><label for="tab1" role="button"><svg class="bi bi-info-circle-fill" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M8 16A8 8 0 108 0a8 8 0 000 16zm.93-9.412l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 100-2 1 1 0 000 2z" />
+                            </svg><br><span>Informações</span></label></li>
+                    <li title="Requisitos"><label for="tab2" role="button"><svg viewBox="0 0 24 24">
                                 <path d="M14,2A8,8 0 0,0 6,10A8,8 0 0,0 14,18A8,8 0 0,0 22,10H20C20,13.32 17.32,16 14,16A6,6 0 0,1 8,10A6,6 0 0,1 14,4C14.43,4 14.86,4.05 15.27,4.14L16.88,2.54C15.96,2.18 15,2 14,2M20.59,3.58L14,10.17L11.62,7.79L10.21,9.21L14,13L22,5M4.93,5.82C3.08,7.34 2,9.61 2,12A8,8 0 0,0 10,20C10.64,20 11.27,19.92 11.88,19.77C10.12,19.38 8.5,18.5 7.17,17.29C5.22,16.25 4,14.21 4,12C4,11.7 4.03,11.41 4.07,11.11C4.03,10.74 4,10.37 4,10C4,8.56 4.32,7.13 4.93,5.82Z" />
-                            </svg><br><span>Descrição</span></label></li>
-                    <li title="Delivery Contents"><label for="tab2" role="button"><svg viewBox="0 0 24 24">
-                                <path d="M2,10.96C1.5,10.68 1.35,10.07 1.63,9.59L3.13,7C3.24,6.8 3.41,6.66 3.6,6.58L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.66,6.72 20.82,6.88 20.91,7.08L22.36,9.6C22.64,10.08 22.47,10.69 22,10.96L21,11.54V16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V10.96C2.7,11.13 2.32,11.14 2,10.96M12,4.15V4.15L12,10.85V10.85L17.96,7.5L12,4.15M5,15.91L11,19.29V12.58L5,9.21V15.91M19,15.91V12.69L14,15.59C13.67,15.77 13.3,15.76 13,15.6V19.29L19,15.91M13.85,13.36L20.13,9.73L19.55,8.72L13.27,12.35L13.85,13.36Z" />
                             </svg><br><span>Requisitos</span></label></li>
-                    <li title="Shipping"><label for="tab3" role="button"><svg viewBox="0 0 24 24">
-                                <path d="M3,4A2,2 0 0,0 1,6V17H3A3,3 0 0,0 6,20A3,3 0 0,0 9,17H15A3,3 0 0,0 18,20A3,3 0 0,0 21,17H23V12L20,8H17V4M10,6L14,10L10,14V11H4V9H10M17,9.5H19.5L21.47,12H17M6,15.5A1.5,1.5 0 0,1 7.5,17A1.5,1.5 0 0,1 6,18.5A1.5,1.5 0 0,1 4.5,17A1.5,1.5 0 0,1 6,15.5M18,15.5A1.5,1.5 0 0,1 19.5,17A1.5,1.5 0 0,1 18,18.5A1.5,1.5 0 0,1 16.5,17A1.5,1.5 0 0,1 18,15.5Z" />
+                    <li title="Contactos"><label for="tab3" role="button"><svg viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 01-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0zm3 1a1 1 0 100-2 1 1 0 000 2z" />
                             </svg><br><span>Contactos</span></label></li>
-                    <li title="Returns"><label for="tab4" role="button"><svg viewBox="0 0 24 24">
-                                <path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
+                    <li title="Experiências"><label for="tab4" role="button"><svg viewBox="0 2 18 18">
+                                <path d="M2.667 3h6.666C10.253 3 11 3.746 11 4.667v6.666c0 .92-.746 1.667-1.667 1.667H2.667C1.747 13 1 12.254 1 11.333V4.667C1 3.747 1.746 3 2.667 3z"/>
+                                <path d="M7.404 8.697l6.363 3.692c.54.313 1.233-.066 1.233-.697V4.308c0-.63-.693-1.01-1.233-.696L7.404 7.304a.802.802 0 000 1.393z"
                             </svg><br><span>Experiências</span></label></li>
+
+                    <!-- Generator: Adobe Illustrator 24.0.1, SVG Export Plug-In  -->
+
+
                 </ul>
 
                 <div class="slider">
@@ -50,15 +60,35 @@ if (mysqli_stmt_prepare($stmt, $query)) {
                 </div>
                 <div class="content">
                     <section>
-                        <h2>Features</h2>
-                        <p><?= $description_vac ?></p>
+                        <h2>Informações</h2>
+
+                        <span class="info_title">Descrição</span>
+                        <p style="font-size: 14px"><?= $description_vac ?></p>
+
+                        <span class="info_title"> Região</span>
+                        <p style="font-size: 14px"><?= $name_region ?></p>
+
+                        <span class="info_title"> Área(s) </span>
+                        <p style="font-size: 14px"><?= $name_interested_area ?></p>
+
+                        <span class="info_title">Habilitações literárias </span>
+                        <p style="font-size: 14px"><?= $name_education?></p>
+
+                        <span class="info_title"> Horário de trabalho</span>
+                        <p style="font-size: 14px"><?= $Workday_name ?></p>
+
+                        <span class="info_title"> Número de vagas disponíveis</span>
+                        <p style="font-size: 14px"><?= $number_free_vanc ?></p>
+
                     </section>
                     <section>
-                        <h2>Delivery Contents</h2>
+                        <h2>Requisitos</h2>
                         <p><?= $requirements ?></p>
+
+
                     </section>
                     <section>
-                        <h2>Shipping</h2>
+                        <h2>Contactos</h2>
                         <?php
                         $query2 = "SELECT name_user, email_user, contact_user, website_ue, facebook_ue, instagram_ue
                     FROM users
@@ -128,7 +158,7 @@ if (mysqli_stmt_prepare($stmt, $query)) {
 
                     </section>
                     <section>
-                        <h2>Returns</h2>
+                        <h2>Experiências</h2>
 
 
                         <div class="card-body">
