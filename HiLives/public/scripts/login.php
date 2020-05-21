ca8101de7b99aceb56d38b6bf6721c939cb6cf00
AnaJVaz <anajvaz@ua.pt>
2020-05-21 17:18:22 +0100

    navbar2

--- HiLives/public/scripts/login.php
+++ HiLives/public/scripts/login.php
@@ -9,14 +9,17 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
 
     $stmt = mysqli_stmt_init($link);
     $stmt2 = mysqli_stmt_init($link2);
-    $query = "SELECT idUser, email_user, password, User_type_idUser_type, active FROM users WHERE email_user LIKE ? ";
+    $query = "SELECT idUser, email_user, password, User_type_idUser_type, active, type_user
+                FROM users 
+                INNER JOIN user_type ON users.User_type_idUser_type = user_type.idUser_type
+                WHERE email_user LIKE ? ";
 
     if (mysqli_stmt_prepare($stmt, $query)) {
         mysqli_stmt_bind_param($stmt, 's', $email_user);
         $email_user = $_POST['email'];
         $password = $_POST['password'];
         mysqli_stmt_execute($stmt);
-        mysqli_stmt_bind_result($stmt, $idUser, $email_user, $passwordx, $User_type, $active);
+        mysqli_stmt_bind_result($stmt, $idUser, $email_user, $passwordx, $User_type, $active, $type_user);
 
         if (mysqli_stmt_fetch($stmt)) {
             if (password_verify($password, $passwordx)) {
@@ -31,7 +34,14 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     //Aqui era onde tínhamos o script do match
                     // feedback de sucesso
                     include "match_uni_login.php";
-                    header("Location: ../home_people.php");
+                    echo "$type_user";
+                    if($type_user == "Jovem") {
+                        header("Location: ../home_people.php");
+                    } else if ($type_user == "Empresa") {
+                        header("Location: ../home_companies.php");
+                    } else if ($type_user == "Universidade") {
+                        header("Location: ../home_uni.php");
+                    }
                     echo "LOGIN DEU";
                 } else {
                     // header("Location: ../login.php?msg=1");
