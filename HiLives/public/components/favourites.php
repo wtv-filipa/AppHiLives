5b75e116f4b41ead694335e16e6aa466c0b178ab
Fred <fredericoproenca@ua.pt>
2020-05-25 10:44:27 +0100

    html favoritos

---
+++ HiLives/public/components/favourites.php
@@ -0,0 +1,88 @@
+
+<div class="w-75 mx-auto list_links">
+    <div id='wrapper_title'>
+        <div class='tagpost-top section' id='tagpost-top'>
+            <div class='widget HTML' id='HTML5'>
+                <div data-aos="fade-up">
+                    <h3 class="mb-4 main_title adjustment_top">Os meus favoritos</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id='wrapper'>
+        <div id='recenttags'>
+            <div class='tagpost-top section' id='tagpost-top'>
+                <div class='widget HTML' id='HTML5'>
+                    <div class='widget-content'>
+                        <ul class='taglabel'>
+
+                            <li class='clearfix_companies'>
+                                <a href=""><img alt="Imagem da FNAC2" title="" class="tagpost_thumb"
+                                                src="img/fnac.jpg"></a>
+                                <p class="mb-0 link_info"><i class="fa fa-briefcase mr-1"
+                                                             aria-hidden="true"></i>Trabalhar
+                                </p>
+                                <h4 class="mb-0 link_title">FNAC</h4>
+                                <h5 class="mb-0 link_subtitle">Recursos Humanos</h5>
+
+                            </li>
+                            <li class='clearfix_companies'>
+                                <a href=""><img alt="Imagem da FNAC2" title="" class="tagpost_thumb"
+                                                src="img/fnac.jpg"></a>
+                                <p class="mb-0 link_info"><i class="fa fa-briefcase mr-1"
+                                                             aria-hidden="true"></i>Trabalhar
+                                </p>
+                                <h4 class="mb-0 link_title">FNAC</h4>
+                                <h5 class="mb-0 link_subtitle">Recursos Humanos</h5>
+                            </li>
+                            <li class='clearfix_companies'>
+                                <a href=""><img alt="Imagem da FNAC2" title="" class="tagpost_thumb"
+                                                src="img/fnac.jpg"></a>
+                                <p class="mb-0 link_info"><i class="fa fa-briefcase mr-1"
+                                                             aria-hidden="true"></i>Trabalhar
+                                </p>
+                                <h4 class="mb-0 link_title">FNAC</h4>
+                                <h5 class="mb-0 link_subtitle">Recursos Humanos</h5>
+                            </li>
+                            <li class='clearfix_companies'>
+                                <a href=""><img alt="Imagem da FNAC2" title="" class="tagpost_thumb"
+                                                src="img/fnac.jpg"></a>
+                                <p class="mb-0 link_info"><i class="fa fa-briefcase mr-1"
+                                                             aria-hidden="true"></i>Trabalhar
+                                </p>
+                                <h4 class="mb-0 link_title">FNAC</h4>
+                                <h5 class="mb-0 link_subtitle">Recursos Humanos</h5>
+                            </li>
+                            <li class='clearfix_companies'>
+                                <a href=""><img alt="Imagem da FNAC2" title="" class="tagpost_thumb"
+                                                src="img/fnac.jpg"></a>
+                                <p class="mb-0 link_info"><i class="fa fa-briefcase mr-1"
+                                                             aria-hidden="true"></i>Trabalhar
+                                </p>
+                                <h4 class="mb-0 link_title">FNAC</h4>
+                                <h5 class="mb-0 link_subtitle">Recursos Humanos</h5>
+                            </li>
+                            <li class='clearfix_companies'>
+                                <a href=""><img alt="Imagem da FNAC2" title="" class="tagpost_thumb"
+                                                src="img/fnac.jpg"></a>
+                                <p class="mb-0 link_info"><i class="fa fa-briefcase mr-1"
+                                                             aria-hidden="true"></i>Trabalhar
+                                </p>
+                                <h4 class="mb-0 link_title">FNAC</h4>
+                                <h5 class="mb-0 link_subtitle">Recursos Humanos</h5>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+<!--        <div id='footer'>-->
+<!--            <a href="can_choose_work.php">-->
+<!--                <button class="btn_destaques2 card_btn mx-auto">Ver mais</button>-->
+<!--            </a>-->
+<!--        </div>-->
+    </div>
+</div>
+
+
