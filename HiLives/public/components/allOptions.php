c6f16d9c0623d7566d225e7762503f11d25b8740
AnaJVaz <anajvaz@ua.pt>
2020-07-02 15:49:00 +0100

    estudar e trabalhar favoritos

--- HiLives/public/components/allOptions.php
+++ HiLives/public/components/allOptions.php
@@ -1,171 +1,115 @@
 <?php
 include "navbar_2.php";
+
+
+$query = "SELECT id_match, User_university, Area, name_user, profile_img, favorite 
+            FROM young_university 
+            INNER JOIN users ON young_university.User_university = users.idUser";
+
+$query2 = "SELECT id_match_vac, User_young, Vacancies_idVacancies, match_perc, favorite, profile_img, vacancie_name, name_user
+            FROM user_has_vacancies
+            INNER JOIN vacancies ON user_has_vacancies.Vacancies_idVacancies = vacancies.idVacancies 
+            INNER JOIN users ON vacancies.User_publicou = users.idUser";
+
+
 ?>
-<!--UNIVERSIDADES E EMPRESAS-->
+<!--UNIVERSIDADES-->
 <div class=" mx-auto div_geral2">
     <div id='wrapper_title' class="mb-4">
         <div class='tagpost-top section' id='tagpost-top'>
             <div class='widget HTML' id='HTML5'>
                 <div>
-                    <h3 class="main_title">Todas as opções disponíveis</h3>
+                    <h3 class="main_title">Todas as opções disponíveis | Estudar </h3>
                 </div>
             </div>
         </div>
     </div>
-
-
     <div class="card-deck text-center row">
-
+        <?php
+        if (mysqli_stmt_prepare($stmt, $query)) {
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $id_match, $User_university, $Area, $name_user, $profile_img, $favorite);
+        while (mysqli_stmt_fetch($stmt)) {
+        ?>
         <div class="cards col-xs-12 col-sm-6 col-lg-4">
             <div class="card-item">
                 <div class="card-image">
-                    <img class="imagem" src="img/ua.jpg"></div>
+                    <?php
+                    if (isset($profile_img)) {
+                        ?>
+                        <img class="imagem_db" src="../admin/uploads/img_perfil/<?= $profile_img ?>"
+                             alt="<?= $profile_img ?>"/>
+                        <?php
+                    } else {
+                        ?>
+                        <img class="imagem" src="img/INDEX_2.jpg" alt="sem imagem de perfil"/>
+                        <?php
+                    }
+                    ?>
+                </div>
                 <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
-                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
-                    <p class="card-intro description_title2">Ver áreas disponíveis</p>
+                    <h4 class="card-intro description_title">
+                        <i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar
+                    </h4>
+                    <h2 class="card-title sub_title"><?= $name_user ?></h2>
+                    <a href="profile.php?user=<?= $User_university ?>" class="btn_cards">Ver áreas disponíveis</a>
                 </div>
             </div>
         </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/fnac.jpg">
-                </div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fa fa-briefcase mr-1" style="color: #2f2f2f;" aria-hidden="true"></i> Trabalhar</h4>
-                    <h2 class="card-title sub_title">FNAC </h2>
-                    <p class="card-intro description_title2">Ver vagas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/ua.jpg"></div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
-                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
-                    <p class="card-intro description_title2">Ver áreas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/fnac.jpg">
-                </div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fa fa-briefcase mr-1" style="color: #2f2f2f;" aria-hidden="true"></i> Trabalhar</h4>
-                    <h2 class="card-title sub_title">FNAC</h2>
-                    <p class="card-intro description_title2">Ver vagas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/ua.jpg"></div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
-                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
-                    <p class="card-intro description_title2">Ver áreas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/fnac.jpg">
-                </div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fa fa-briefcase mr-1" style="color: #2f2f2f;" aria-hidden="true"></i> Trabalhar</h4>
-                    <h2 class="card-title sub_title">FNAC</h2>
-                    <p class="card-intro description_title2">Ver vagas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/ua.jpg"></div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
-                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
-                    <p class="card-intro description_title2">Ver áreas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/fnac.jpg">
-                </div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fa fa-briefcase mr-1" style="color: #2f2f2f;" aria-hidden="true"></i> Trabalhar</h4>
-                    <h2 class="card-title sub_title">FNAC</h2>
-                    <p class="card-intro description_title2">Ver vagas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/fnac.jpg">
-                </div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fa fa-briefcase mr-1" style="color: #2f2f2f;" aria-hidden="true"></i> Trabalhar</h4>
-                    <h2 class="card-title sub_title">FNAC</h2>
-                    <p class="card-intro description_title2">Ver vagas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/ua.jpg"></div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
-                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
-                    <p class="card-intro description_title2">Ver áreas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/ua.jpg"></div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar</h4>
-                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
-                    <p class="card-intro description_title2">Ver áreas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="cards col-xs-12 col-sm-6 col-lg-4">
-            <div class="card-item">
-                <div class="card-image">
-                    <img class="imagem" src="img/ua.jpg">
-                </div>
-                <div class="card-info">
-                    <h4 class="card-intro description_title"><i class="fas fa-book-open" style="color: #2f2f2f;"></i> Estudar</h4>
-                    <h2 class="card-title sub_title">Universidade de Aveiro</h2>
-                    <p class="card-intro description_title2">Ver áreas disponíveis</p>
-                </div>
-            </div>
-        </div>
-
+        <?php
+        }
+        }
+        ?>
     </div>
-    <!--fim do que engloba os cards-->
-</div> <!-- div da w-75-->
+</div>
+
+
+
+<div class=" mx-auto div_geral2">
+    <div id='wrapper_title' class="mb-4">
+        <div class='tagpost-top section' id='tagpost-top'>
+            <div class='widget HTML' id='HTML5'>
+                <div>
+                    <h3 class="main_title">Todas as opções disponíveis | Trabalhar </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card-deck text-center row">
+        <?php
+        if (mysqli_stmt_prepare($stmt, $query2)) {
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt, $id_match_vac, $User_young, $Vacancies_idVacancies, $match_perc, $favorite, $profile_img, $vacancie_name, $name_user);
+            while (mysqli_stmt_fetch($stmt)) {
+                ?>
+                <div class="cards col-xs-12 col-sm-6 col-lg-4">
+                    <div class="card-item">
+                        <div class="card-image">
+                            <?php
+                            if (isset($profile_img)) {
+                                ?>
+                                <img class="imagem_db" src="../admin/uploads/img_perfil/<?= $profile_img ?>"
+                                     alt="<?= $profile_img ?>"/>
+                                <?php
+                            } else {
+                                ?>
+                                <img class="imagem" src="img/INDEX_2.jpg" alt="sem imagem de perfil"/>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                        <div class="card-info">
+                            <h4 class="card-intro description_title">
+                                <i class="fas fa-book" style="color: #2f2f2f;"></i> Estudar
+                            </h4>
+                            <h2 class="card-title sub_title"><?= $vacancie_name ?></h2>
+                            <a href="vacancie.php?vac=<?= $Vacancies_idVacancies ?>" class="btn_cards">Ver áreas disponíveis</a>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+        }
+        ?>
+    </div>
+</div>
--- HiLives/public/components/favorites.php
+++ HiLives/public/components/favorites.php
@@ -1,101 +1,197 @@
-
-
 <?php
 include "navbar_2.php";
 
-
 if ($_SESSION["idUser"]) {
+$idUser = $_SESSION["idUser"];
 
-    $idUser = $_SESSION["idUser"];
+require_once("connections/connection.php");
 
-    require_once("connections/connection.php");
+$link = new_db_connection();
+$stmt = mysqli_stmt_init($link);
 
-    $link = new_db_connection();
-    $stmt = mysqli_stmt_init($link);
-
-    $query = "SELECT Area, name_user, profile_img, id_match, favorite
-            FROM young_university 
+$query = "SELECT Area, name_user, profile_img, id_match, favorite, idUser
+            FROM young_university
             INNER JOIN users ON young_university.User_university = users.idUser
-            WHERE User_young LIKE ? AND favorite=1";
+            WHERE User_young = ? AND favorite=1";
 
-    ?>
-
-
-    <div class="w-75 mx-auto list_links">
+$query2 = "SELECT id_match_vac, User_young, Vacancies_idVacancies, match_perc, favorite, vacancie_name, name_user, profile_img
+            FROM user_has_vacancies
+            INNER JOIN vacancies ON user_has_vacancies.Vacancies_idVacancies = vacancies.idVacancies
+            INNER JOIN users ON vacancies.User_publicou = users.idUser
+            WHERE User_young = ? AND favorite=1";
+?>
+<div class="w-75 mx-auto list_links">
     <div id='wrapper_title'>
         <div class='tagpost-top section' id='tagpost-top'>
             <div class='widget HTML' id='HTML5'>
                 <div data-aos="fade-up">
-                    <h3 class="mb-4 main_title">Os meus favoritos</h3>
+                    <h3 class="mb-4 main_title">Os meus favoritos de estudar</h3>
                 </div>
             </div>
         </div>
     </div>
-
     <div id='wrapper'>
-    <div id='recenttags'>
-    <div class='tagpost-top section' id='tagpost-top'>
-    <div class='widget HTML' id='HTML5'>
-    <div class='widget-content'>
+        <div id='recenttags'>
+            <div class='tagpost-top section' id='tagpost-top'>
+                <div class='widget HTML' id='HTML5'>
+                    <div class='widget-content'>
+                        <ul class='taglabel'>
+                            <?php
+                            if (mysqli_stmt_prepare($stmt, $query)) {
+                                mysqli_stmt_bind_param($stmt, 'i', $idUser);
+                                mysqli_stmt_execute($stmt);
+                                mysqli_stmt_bind_result($stmt, $Area, $name_user, $profile_img, $id_match, $favorite, $idUser1);
+                                while (mysqli_stmt_fetch($stmt)) {
+                                    ?>
+                                    <a href="profile.php?user=<?= $idUser1 ?>">
+                                        <li class='clearfix_uni mt-2'>
+                                            <?php
+                                            if ($favorite == 0) {
+                                                echo "";
+                                                ?>
+                                            <a href="scripts/update_fav.php?match=<?= $id_match ?>&fav=<?= $favorite ?>">
+                                                <button class="btn rounded-circle btn_fav">
+                                                    <i class="fa fa-heart-o" aria-hidden="true"
+                                                       style="color: #2F2F2F"></i>
+                                                </button>
+                                            </a>
+                                            <?php
+                                            } else {
+                                            ?>
+                                            <a href="scripts/update_fav.php?match=<?= $id_match ?>&fav=<?= $favorite ?>">
+                                                <button class="btn rounded-circle btn_fav">
+                                                    <i class="fa fa-heart" aria-hidden="true"
+                                                       style="color: #A31621"></i>
+                                                </button>
+                                            </a>
+                                            <?php
+                                            }
+                                            if (isset($profile_img)) {
+                                                ?>
+                                                <img alt="" title="" class="tagpost_thumb" src="../admin/uploads/img_perfil/<?= $profile_img ?>">
+                                                <?php
+                                            } else {
+                                                ?>
+                                                <img alt="" title="" class="tagpost_thumb" src="img/index_2.jpg">
+                                                <?php
+                                            }
+                                            ?>
+                                            <p class="mb-0 link_info">
+                                                <i class="fas fa-book" aria-hidden="true"></i> Estudar
+                                            </p>
+                                            <a href='profile.php?user=<?= $idUser1 ?>'>
+                                                <h4 class="mb-0 link_title"><?= $name_user ?></h4>
+                                                <h5 class="mb-0 link_subtitle"><?= $Area ?></h5>
+                                            </a>
+                                        </li>
+                                    </a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-            <ul class='taglabel'>
-    <?php
-    if (mysqli_stmt_prepare($stmt, $query)) {
 
-        mysqli_stmt_bind_param($stmt, 'i', $idUser);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_bind_result($stmt, $Area, $name_user, $profile_img, $id_match, $favorite);
-
-        while (mysqli_stmt_fetch($stmt)) {
-            ?>
-                <li class='clearfix_uni mt-2'>
-                    <a href="">
+<div class="w-75 mx-auto list_links">
+    <div id='wrapper_title'>
+        <div class='tagpost-top section' id='tagpost-top'>
+            <div class='widget HTML' id='HTML5'>
+                <div data-aos="fade-up">
+                    <h3 class="mb-4 main_title">Os meus favoritos de trabalhar</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id='wrapper'>
+        <div id='recenttags'>
+            <div class='tagpost-top section' id='tagpost-top'>
+                <div class='widget HTML' id='HTML5'>
+                    <div class='widget-content'>
+                        <ul class='taglabel'>
                         <?php
-                        if (isset($profile_img)) {
-                            ?>
-                            <img alt="<?= $profile_img ?>" title="" class="tagpost_thumb"
-                                 src="../uploads/img_perfil/<?= $profile_img ?>">
-                            <?php
-                        } else {
-                            ?>
-                            <img alt="imagem de perfil default da universidade" title="" class="tagpost_thumb"
-                                 src="img/index_2.jpg">
-                            <?php
+                        if (mysqli_stmt_prepare($stmt, $query2)) {
+                            mysqli_stmt_bind_param($stmt, 'i', $idUser);
+                            mysqli_stmt_execute($stmt);
+                            mysqli_stmt_bind_result($stmt, $id_match_vac, $User_young, $Vacancies_idVacancies, $match_perc, $favorite, $vacancie_name, $name_user_emp, $profile_img_emp);
+                            while (mysqli_stmt_fetch($stmt)) {
+
+                                if($match_perc == 1){
+                                    ?>
+                                    <a href='vacancie.php?vac=<?= $Vacancies_idVacancies ?>'>
+                                    <?php
+                                } else {
+                                    ?>
+                                        <a href='vacancie_learn.php?vac=<?= $Vacancies_idVacancies ?>'>
+                                            <?php
+                                }
+                                ?>
+                                <li class='clearfix_uni mt-2'>
+                                    <?php
+                                    if ($favorite == 0) {
+                                        echo "";
+                                        ?>
+                                        <a href="scripts/update_fav.php?m=<?= $id_match_vac ?>&f=<?= $favorite ?>">
+                                            <button class="btn rounded-circle btn_fav">
+                                                <i class="fa fa-heart-o" aria-hidden="true"
+                                                   style="color: #2F2F2F"></i>
+                                            </button>
+                                        </a>
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <a href="scripts/update_fav.php?m=<?= $id_match_vac ?>&f=<?= $favorite ?>">
+                                            <button class="btn rounded-circle btn_fav">
+                                                <i class="fa fa-heart" aria-hidden="true"
+                                                   style="color: #A31621"></i>
+                                            </button>
+                                        </a>
+                                        <?php
+                                    }
+                                    if (isset($profile_img)) {
+                                        ?>
+                                        <img alt="" title="" class="tagpost_thumb" src="../admin/uploads/img_perfil/<?=$profile_img_emp?>">
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <img alt="" title="" class="tagpost_thumb" src="img/index_3.jpg">
+                                        <?php
+                                    }
+                                    ?>
+                                    <p class="mb-0 link_info">
+                                        <i class="fas fa-suitcase" aria-hidden="true"></i> Trabalhar
+                                    </p>
+                                    <?php
+                                    if($match_perc == 1){
+                                    ?>
+                                    <a href='vacancie.php?vac=<?= $Vacancies_idVacancies ?>'>
+                                        <?php
+                                        } else {
+                                        ?>
+                                        <a href='vacancie_learn.php?vac=<?= $Vacancies_idVacancies ?>'>
+                                            <?php
+                                            }
+                                    ?>
+                                        <h4 class="mb-0 link_title"><?= $vacancie_name ?></h4>
+                                        <h5 class="mb-0 link_subtitle"><?= $name_user_emp ?></h5>
+                                    </a>
+                                </li>
+                            </a>
+                                <?php
+                            }
                         }
-                        ?>
-                        <?php
-
-                        if ($favorite == 0){
-                            ?>
-                            <a href="scripts/update_fav.php?match=<?= $id_match?>&fav=<?= $favorite?>"> <button class="btn rounded-circle btn_fav"><i class="fa fa-heart-o" aria-hidden="true"  style="color: #2F2F2F"></i></button></a>
-                            <?php
-                        } else {
-                            ?>
-                            <a href="scripts/update_fav.php?match=<?= $id_match?>&fav=<?= $favorite?>"> <button class="btn rounded-circle btn_fav"><i class="fa fa-heart" aria-hidden="true" style="color: #A31621"></i> </button></a>
-                            <?php
-                        }
-                        ?>
-
-                        <p class="mb-0 link_info"><i class="fa fa-book mr-1" aria-hidden="true"></i>Estudar
-                        </p>
-                        <h4 class="mb-0 link_title"><?= $name_user ?></h4>
-                        <h5 class="mb-0 link_subtitle"><?= $Area ?></h5>
-                    </a>
-                </li>     <?php
-        }
-        ?>
-            </ul>
-
+                                ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class='clear'></div>
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-        <?php
-    }
-} else {
-    include("404.php");
+    </div>
+</div>
+<?php
 }
-?>
